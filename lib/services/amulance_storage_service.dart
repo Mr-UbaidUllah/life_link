a5c2416e56ba591de9706fb4708edf6027b07224
Mr-UbaidUllah <ubaidullah.dev09@gmail.com>
2026-06-10 19:39:15 +0500

    fix: update ambulance storage service

--- lib/services/amulance_storage_service.dart
+++ lib/services/amulance_storage_service.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class AmbulanceStorageService {
-    final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
   // final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-   Future<String> uploadorganizationImage(String uid, File image) async {
+  Future<String> uploadorganizationImage(String uid, File image) async {
     final ref = _storage.ref().child('ambulance_images/$uid.jpg');
 
     await ref.putFile(image);
