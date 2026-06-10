e6e6d4251be19107e617557f96bd3dfb1f5150c6
Mr-UbaidUllah <ubaidullah.dev09@gmail.com>
2026-06-10 19:07:46 +0500

    recent changes updated

---
+++ lib/core/constants/firebase_constants.dart
@@ -0,0 +1,10 @@
+class FirebaseConstants {
+  static const String users = 'users';
+  static const String bloodRequests = 'Blood_request';
+  static const String chats = 'chats';
+  static const String messages = 'messages';
+  static const String notifications = 'notifications';
+  static const String organizations = 'organizations';
+  static const String volunteer = 'Volunteer';
+  static const String ambulance = 'Ambulance';
+}
